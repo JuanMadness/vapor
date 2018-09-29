50f1dc3d2f2e108d4766e03a6649031e03ee0089
JuanMadness <sebastianrenzer@gmail.com>
2018-09-29 21:20:59 +0200

    -Print now gamename and appid

--- src/vapor/CommandLineLogic.java
+++ src/vapor/CommandLineLogic.java
@@ -1,8 +1,12 @@
 package vapor;
 
+import vapor.entities.Game;
+import vapor.entities.SteamLibrary;
 import vapor.tools.Sdout;
 import vapor.tools.SteamUtility;
 import vapor.tools.SystemInfo;
+
+import java.util.List;
 
 public class CommandLineLogic {
 
@@ -11,11 +15,25 @@ public class CommandLineLogic {
     public CommandLineLogic() {
         //init
         systemInfo = new SystemInfo();
-        Sdout.isDebugEnabled = true;
+        Sdout.isDebugEnabled = false;
 
         //weitere logik
+        //SteamCmdUtility.textSteamCmd();
+        //SteamCmdUtility.updateAllGames(SteamUtility.getSteamLibraries());
 
-        SteamUtility.getSteamLibraries();
+        printAllLibs();
+    }
+
+    public void printAllLibs() {
+        List<SteamLibrary> tSteamLibs = SteamUtility.getSteamLibraries();
+
+        for (SteamLibrary tLib : tSteamLibs) {
+            System.out.println("+++++++++++++++++++++++++++++++++++++++++++");
+            System.out.println(tLib.getPath());
+            for (Game tGame : tLib.getGames()) {
+                System.out.printf("%-50s %-10s%n", tGame.getName(), tGame.getAppID());
+            }
+        }
     }
 
 }
--- src/vapor/tools/SteamCmdUtility.java
+++ src/vapor/tools/SteamCmdUtility.java
@@ -1,18 +1,69 @@
 package vapor.tools;
 
 import vapor.entities.Game;
+import vapor.entities.SteamLibrary;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SteamCmdUtility {
 
+    //cmd="steamcmd +login $user $userPw"
+    //+app_update appID
+    //+quit
+
+    public static String steamCmdPath = "C:\\Program Files (x86)\\Steam\\steamcmd.exe";
+
     private static void runSteamCmd(String pCmd) {
         //führt SteamCmd mit parameter aus
+        StringBuilder tCmd = new StringBuilder();
+        tCmd.append(" +login test +ShutdownOnFailedCommand 1 ");
+        tCmd.append(pCmd);
+        tCmd.append("+exit");
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder(steamCmdPath, tCmd.toString());
+            processBuilder.inheritIO();
+
+
+            Process process = processBuilder.start();
+            process.waitFor();
+        } catch (IOException e) {
+            Sdout.error("IOException: runSteamCmd");
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            Sdout.error("InterruptedException: runSteamCmd");
+        }
     }
 
-    public static void updateAllGames() {
+    public static void updateAllGames(List<SteamLibrary> pSteamLibs) {
         //erstellt Befehlt um alle Spiele in den Libs zu updaten
+        List<String> tAppIds = new ArrayList<String>();
+
+        for (SteamLibrary tSteamLib : pSteamLibs) {
+            for (Game tGame : tSteamLib.getGames()) {
+                tAppIds.remove(tGame.getAppID());
+                tAppIds.add(tGame.getAppID());
+            }
+        }
+
+        StringBuilder tCmd = new StringBuilder();
+        for (String tString : tAppIds) {
+            tCmd = tCmd.append("+app_update " + tString + " ");
+        }
+
+        runSteamCmd(tCmd.toString());
     }
 
     public static void updateGame(Game pGame) {
         //erstellt befehl um ein Spiel zu updaten/hinzuzufügen
+    }
+
+    public static void addNewGame(String pAppId, String pUniverse) {
+        //downloads new game
+    }
+
+    public static void textSteamCmd() {
+        runSteamCmd("+app_update 500710 ");
     }
 }
